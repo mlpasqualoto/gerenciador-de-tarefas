79ae431294cb660ae9e16d6a70efb5d2c8ffc6fd
Matheus Leite Pasqualoto <matheus_pasqualoto@outlook.com>
2025-01-20 11:52:02 -0300

    Rotas atualizadas e banco de dados configurado

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,10 +1,21 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient, ServerApiVersion } from 'mongodb';
+import dotenv from 'dotenv';
 
-export default async function connectToDatabase(stringConexao) {
-    let mongoClient;
+dotenv.config();
 
+let mongoClient;
+
+export default async function connectToDatabase() {
+    const uri = `mongodb+srv://matheuspasqualoto:${process.env.DB_PASSWORD}@taskmanager.whkyz.mongodb.net/?retryWrites=true&w=majority&appName=taskManager`;
+    
     try {
-        mongoClient = new MongoClient(stringConexao);
+        mongoClient = new MongoClient(uri, {
+            serverApi: {
+                version: ServerApiVersion.v1,
+                strict: true,
+                deprecationErrors: true,
+            }
+        });
         console.log('Conectando ao cluster do banco de dados...');
 
         await mongoClient.connect();
@@ -13,6 +24,6 @@ export default async function connectToDatabase(stringConexao) {
         return mongoClient;
     } catch (error) {
         console.log('Erro ao conectar no banco de dados', error);
-        process.exit();
+        throw new Error('Não foi possível conectar ao MongoDB.');
     };
 };
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,8 @@
 import express from "express";
+import connectToDatabase from "../config/dbConfig.js";
 
 const routes = (app) => {
+    connectToDatabase();
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
 
@@ -8,10 +10,19 @@ const routes = (app) => {
         res.json({ message: "Welcome to the API" });
     });
 
-    app.post("/register", (req, res) => {
+    app.post("/register", async (req, res) => {
         const { user, email, password } = req.body;
 
-        res.json({ message: "Registrado com sucesso!" });
+        try {
+            // Insere os dados no MongoDB
+            const result = await collection.insertOne({ user, email, password });
+            console.log('Documento inserido:', result);
+
+            res.status(201).json({ message: 'Usuário cadastrado com sucesso!' });
+        } catch (error) {
+            console.error('Erro ao salvar no MongoDB:', error);
+            res.status(500).json({ message: 'Erro interno no servidor.' });
+        }
     });
 };
 
