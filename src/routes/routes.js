6031b0f41d030121745510a9c0a672605e925daf
Matheus <matheus_pasqualoto@outlook.com>
2025-01-21 01:11:50 -0300

    Funções de Login e Registro concluídas

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,7 @@
 import express from "express";
 import connectToDatabase from "../config/dbConfig.js";
+import bcrypt from "bcrypt";
+import { error } from "console";
 
 const routes = (app) => {
     app.use(express.json());
@@ -18,13 +20,45 @@ const routes = (app) => {
             const db = mongoClient.db("taskManager"); // Nome do banco
             const collection = db.collection("users"); // Nome da coleção
 
+            // Criptografa a senha antes de salvar
+            const hashedPassword = await bcrypt.hash(password, 10);
+
             // Insere os dados no MongoDB
-            const result = await collection.insertOne({ name, email, password });
+            const result = await collection.insertOne({ name, email, password: hashedPassword });
             console.log("Documento inserido:", result);
 
             res.status(201).json({ message: "Usuário cadastrado com sucesso!" });
         } catch (error) {
             console.error("Erro ao salvar no MongoDB:", error);
+            res.status(500).json({ message: "Erro interno no servidor." });
+        }
+    });
+
+    app.post("/login", async (req, res) => {
+        const { name, password } = req.body;
+
+        try {
+            const mongoClient = await connectToDatabase();
+            const db = mongoClient.db("taskManager");
+            const collection = db.collection("users");
+
+            const user = await collection.findOne({ name });
+
+            if (!user) {
+                return res.status(404).json({ error: "Usuário não encontrado." });
+            }
+
+            // Verifica a senha
+            const isPasswordValid = await bcrypt.compare(password, user.password);
+
+            if (!isPasswordValid) {
+                return res.status(401).json({ error: "Credenciais inválidas." });
+            }
+
+            // Caso sucesso
+            res.status(200).json({ message: "Login bem-sucedido!", user: { name: user.name, email: user.email } })
+        } catch (error) {
+            console.error("Erro ao buscar no MongoDB:", error);
             res.status(500).json({ message: "Erro interno no servidor." });
         }
     });
